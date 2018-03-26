9a1bb0b8e1ab1324c21f95d5cfa74c3d8a6c95f0
theamitabh <amitprem@yahoo.com>
2018-03-26 16:38:32 +0400

    Update server.js

--- server.js
+++ server.js
@@ -5,54 +5,50 @@ let app = express();
 const PORT = 3000;
 
 //Defaults 7545 for Ganache , 8545 for testrpc/Ganache-cli
-var providerLocation='http://localhost:7545';
+var providerLocation = 'http://localhost:7545';
 
 
 const Web3 = require('web3');
 let web3 = new Web3();
-
-
 web3.setProvider(new Web3.providers.HttpProvider(providerLocation));
 
 app.use(express.static('public')); //public folder for static content
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile('index.html');
- });
- 
-
-// localhost:3000/blockchain
-app.get('/blockchain', function(req,res){
-   web3.eth.getAccounts(function(err, accounts){
-      if(err == null) res.send(JSON.stringify(accounts));
-   });
 });
 
+
+// localhost:3000/accounts
+app.get('/accounts', function (req, res) {
+    web3.eth.getAccounts(function (err, accounts) {
+        if (err == null) res.json(accounts);
+    });
+});
+
+
 // Rest API for endpoint localhost:3000/transaction
-app.get('/transaction', function(req,res){
+app.get('/txcount', function (req, res) {
     var obj = web3.eth.getBlockTransactionCount("latest")
-.then(console.log);
-    //var transaction = web3.eth.getTransactionFromBlock('latest').then(console.log);
-    //res.send(JSON.stringify(transaction));  
-    res.send(JSON.stringify(obj)); 
+        .then(console.log);
+    res.send(JSON.stringify(obj));
 
 });
 
 // /block - REST API to get a block 
-app.get('/block', function(req,res){
+app.get('/block', function (req, res) {
     var blocknum = 0; // fetching block 0 for test 
-    web3.eth.getBlock(blocknum, function(error, block){
-        if (!error){
-            console.log("retrieved")        ;
-            res.send(JSON.stringify(block)); 
-        } else{
+    web3.eth.getBlock(blocknum, function (error, block) {
+        if (!error) {
+            res.json(block);
+        } else {
             console.error(error);
         }
     });
 });
 
- 
 
-app.listen(PORT, function(){
-   console.log('Server is started on port:',PORT);
+
+app.listen(PORT, function () {
+    console.log('Server is started on port:', PORT);
 });
