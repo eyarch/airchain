cbe6b888adabc4a8b18361441ab7976904ed938d
theamitabh <amitprem@yahoo.com>
2018-03-26 12:18:26 +0400

    Updated for testrpc and ganache cli

--- server.js
+++ server.js
@@ -4,21 +4,27 @@ const express = require('express');
 let app = express();
 const PORT = 3000;
 
+//Defaults 7545 for Ganache , 8545 for testrpc/Ganache-cli
+var providerLocation='http://localhost:7545';
+
 const Web3 = require('web3');
 let web3 = new Web3();
 
-web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+web3.setProvider(new Web3.providers.HttpProvider(providerLocation));
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
 });
 
+// localhost:3000/blockchain
 app.get('/blockchain', function(req,res){
    web3.eth.getAccounts(function(err, accounts){
       if(err == null) res.send(JSON.stringify(accounts));
    });
 });
 
+// Rest API for endpoint localhost:3000/transaction
 app.get('/transaction', function(req,res){
     var obj = web3.eth.getBlockTransactionCount("latest")
 .then(console.log);
