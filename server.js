8111b3c56002704a61ba6caa748baed4cb37d25e
theamitabh <amitprem@yahoo.com>
2018-03-28 20:25:40 +0400

    changed /txn and /block APIs to accept specifics

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ let app = express();
 const PORT = 3000;
 
 //Defaults 7545 for Ganache , 8545 for testrpc/Ganache-cli
-var providerLocation = 'http://localhost:7545';
-
+var providerLocation = 'http://localhost:8545';
 
 const Web3 = require('web3');
 let web3 = new Web3();
@@ -19,14 +18,23 @@ app.get('/', function (req, res) {
 });
 
 
-// localhost:3000/accounts
+// localhost:3000/accounts - get all accounts
 app.get('/accounts', function (req, res) {
     web3.eth.getAccounts(function (err, accounts) {
         if (err == null) res.json(accounts);
     });
 });
 
+// get a specific transaction
+// txn/0x0a41ec0ac9cbdb224ff5db91a90b030727788f9a3aebf191fc568aa4358582b5
+app.get('/txn/:txhash', function (req, res) {
+    web3.eth.getTransaction(req.params.txhash, function (err, txn) {
+        if (err == null) res.json(txn);
+    });
+    
+});
 
+//
 // Rest API for endpoint localhost:3000/transaction
 app.get('/txcount', function (req, res) {
     var obj = web3.eth.getBlockTransactionCount("latest")
@@ -35,10 +43,9 @@ app.get('/txcount', function (req, res) {
 
 });
 
-// /block - REST API to get a block 
-app.get('/block', function (req, res) {
-    var blocknum = 0; // fetching block 0 for test 
-    web3.eth.getBlock(blocknum, function (error, block) {
+// /block - REST API to get a block  user /block/0 to get 0 block
+app.get('/block/:blocknum', function (req, res) {
+    web3.eth.getBlock(req.params.blocknum, function (error, block) {
         if (!error) {
             res.json(block);
         } else {
